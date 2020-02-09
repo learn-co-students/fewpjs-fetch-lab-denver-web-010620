cfe21826a03e98ff9467ee35697e6326d77895fa
etgreer91 <etgreer91@gmail.com>
2020-02-08 19:15:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => renderBooks(books));
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+function renderBooks(books) {
+  books.map(book => {
+    let h2 = document.createElement('h2')
+    h2.innerText = book.name
+    document.body.appendChild(h2)
   })
 }
 
